c9f1dfeeafd6c9c504b534dd49a1a36a505199b6
Richard Zurad <rzurad@gmail.com>
2015-01-11 18:45:26 -0800

    Detach

---
+++ js/data.js
@@ -0,0 +1,156 @@
+window.rz = {
+    WORKS: [{
+        title: 'TEXTp',
+        imageSrc: './assets/img/textp.png',
+        href: 'https://rzurad.github.io/TEXTp',
+        description: "Built an Ember-CLI app that converts any web-friendly image into <a href=\"http://youtube-global.blogspot.com/2010/03/textp-saves-youtube-bandwidth-money.html\" target=\"_new\">YouTube's 2010 April Fool's Joke</a>.",
+        tags: ['Ember-CLI', 'Web Workers', 'HTML5 Canvas', 'Bootstrap', 'LESS'],
+        date: '2014',
+        className: 'textp'
+    }, {
+        title: 'Waveform',
+        imageSrc: './assets/img/waveform.png',
+        href: 'https://github.com/rzurad/waveform',
+        description: 'Wrote a C program for FreeBSD that generates a waveform image of an audio file using ffmpeg\'s C libraries. Subsequently developed an unhealthy addiction to <a href="https://www.ffmpeg.org/">ffmpeg</a>.',
+        tags: ['C', 'ffmpeg', 'libpng'],
+        date: '2014',
+        className: 'waveform',
+    }, {
+        title: 'Tree of Life JSON',
+        imageSrc: './assets/img/tree-of-life.png',
+        href: 'http://github.com/rzurad/tree-of-life',
+        description: 'Wrote a short Node.js script to convert XML data from the <a href="http://tolweb.org/tree/" target="_new">Tree of Life Web Project</a> into small, more compact JSON that can be loaded into a browser so I could experiment with data visualizations. Learned that 200 MB of XML can be converted into 1 MB of gzipped JSON, subsequently making a snarky comment at XML\'s expense.',
+        tags: ['Node.JS'],
+        date: '2014',
+        className: 'tol'
+    }, {
+        title: 'DS-Extensions',
+        imageSrc: './assets/img/ds-extensions.png',
+        href: 'https://gist.github.com/rzurad/348c436ada5260ad16fe',
+        description: 'Created a proof-of-concept for a shim library that augments <a href="http://emberjs.com/api/data/" target="_new">Ember Data</a> with support for sparse RecordArrays and paginated responses from the persistence layer.',
+        tags: ['Ember Data', 'Ember.JS'],
+        date: '2013',
+        className: 'ds-extensions'
+    }, {
+        title: 'Wiredrive RSS Examples',
+        imageSrc: './assets/img/rss.png',
+        href: 'https://github.com/wiredrive/wiredrive_rss',
+        description: 'Wrote sample code for proxying and consuming <a href="http://www.wiredrive.com">Wiredrive</a> Presentation RSS feeds.',
+        tags: ['PHP', 'jQuery', 'Flash', 'RSS', 'JSONP'],
+        date: '2013',
+        className: 'rss'
+    }, {
+        title: 'Wiredrive Storage Report',
+        imageSrc: './assets/img/wd-storage.jpg',
+        href: 'http://www.wiredrive.com/10806/storage-usage-report/',
+        description: 'Designed and built a usage statistics page to help <a href="http://www.wiredrive.com">Wiredrive</a> administrators keep track of system storage and plan usage.',
+        tags: ['YUI3', 'd3js'],
+        date: '2013',
+        className: 'wd-storage'
+    }, {
+        title: 'Wiredrive Wordpress Player',
+        imageSrc: './assets/img/wiredrive-player.jpg',
+        href: 'http://www.wiredrive.com/11471/updated-wiredrive-player-for-wordpress/',
+        description: 'Built a <a href="http://wordpress.org" target="_new">Wordpress</a> plugin that allows clients to embed <a href="http://www.wiredrive.com">Wiredrive</a> Presentations into Wordpress-powered websites.',
+        tags: ['PHP', 'jQuery', 'Adobe Strobe', 'HTML5 Video'],
+        date: '2013',
+        className: 'wiredrive-player'
+    }, {
+        title: "Wiredrive Reports",
+        imageSrc: './assets/img/wd-reports.png',
+        href: 'http://www.wiredrive.com/9152/software-release-new-wiredrive-reports/',
+        description: 'Helped rewrite the web client of the <a href="http://www.wiredrive.com">Wiredrive</a> reporting application, which allows clients to see layers of analytics information, from system activity to presentation sends/views.',
+        tags: ['YUI3', 'Raphael.JS'],
+        date: '2012',
+        className: 'wd-reports'
+    }, {
+        title: 'Wiredrive Presentations',
+        imageSrc: './assets/img/wd-presentations.jpg',
+        href: 'http://www.wiredrive.com/7611/new-presentations-are-here/',
+        description: 'Helped rebuild the web client of <a href="http://www.wiredrive.com">Wiredrive</a> Presentations, transitioning them from YUI2 to YUI3.',
+        tags: ['YUI3'],
+        date: '2012',
+        className: 'wd-presentations'
+    }, {
+        title: 'Wiredrive Uploader',
+        imageSrc: './assets/img/wd-upload.jpg',
+        href: 'http://www.wiredrive.com/7227/introducing-the-new-html5-uploader/',
+        description: 'Rebuilt the <a href="http://www.wiredrive.com">Wiredrive</a> uploader to allow users to upload files into their Wiredrive system via an HTML5 drag-and-drop interface, Java, or old-school form posts.',
+        tags: ['YUI3'],
+        date: '2012',
+        className: 'wd-upload'
+    }, {
+        title: 'Mahalo.com',
+        imageSrc: './assets/img/mahalo.png',
+        href: 'http://www.mahalo.com',
+        description: 'Helped build the Mahalo 4.0 Django platform for creating and managing how-to content. Built parts of the Wiki back-end and built the web front-end client and admin app for the Mahalo.com homepage.<br><br>While hired to help build out the Django back-end, quickly succumbed to a horrifying JavaScript addiction that, to this day, have yet to recover from.',
+        tags: ['Django', 'MySQL', 'jQuery', 'Backbone.JS'],
+        date: '2011',
+        className: 'Mahalo'
+    }, {
+       title: 'Pong',
+        imageSrc: './assets/img/pong.jpg',
+        description: 'Wrote a Pong clone in Flash that does not believe in angles other than 90 degrees.',
+        tags: ['Flash'],
+        date: '2010',
+        className: 'pong'
+    }, {
+        title: 'Midwest Copier Exchange',
+        imageSrc: './assets/img/mwc.png',
+        href: 'http://www.midwestcopier.com',
+        description: 'Designed and built the web portal for copier wholesaler Midwest Copier Exchange, which consisted of a user portal, a message board, and an inventory tracking system.',
+        tags: ['.NET 2.0', 'MS SQL Server 2005'],
+        date: '2009',
+        className: 'midwest-copier'
+    }, {
+        title: "Black Mesa Flash Site",
+        imageSrc: './assets/img/black-mesa.png',
+        href: 'http://rzurad.github.com/black-mesa-flash-site',
+        description: 'Created the original website for the Half-Life 2 remake project <a href="http://www.blackmesasource.com" target="_new">Black Mesa</a>, and subsequently learned what ranking 3rd place in ModDB\'s 2004 Best Website Award does to one\'s ego.',
+        tags: ['Flash'],
+        date: '2004',
+        className: 'black-mesa'
+    }, {
+        title: 'Influence',
+        imageSrc: './assets/img/influence.jpg',
+        description: 'Made a small <a href="http://store.steampowered.com/app/70/" target="_new">Half-Life Deathmatch</a> map set inside ancient ruins. Map showcases the accidental discovery that rocket launchers in small hallways are fun.',
+        tags: ['Half-Life Deathmatch'],
+        date: '2003',
+        className: 'influence'
+    }, {
+        title: 'ka_moofed',
+        imageSrc: './assets/img/ka_moofed.jpg',
+        description: 'Built a quick knife-only map for <a href="http://store.steampowered.com/app/10/" target="_new">Counter-Strike</a> for a LAN party. Map showcases a complete inability to think about gameplay mechanics and to instead care about overblown easter eggs.',
+        tags: ['Counter-Strike'],
+        date: '2002',
+        className: 'moofed'
+    }, {
+        title: 'cs_pumpo',
+        imageSrc: './assets/img/cs_pumpo.jpg',
+        description: 'Made a pump-shotgun only map for <a href="http://store.steampowered.com/app/10/" target="_new">Counter-Strike</a> inspired by cs_deagle5. Map showcases the only map I made that was actually fun to play.',
+        tags: ['Counter-Strike'],
+        date: '2002',
+        className: 'pumpo',
+    }, {
+        title: 'cs_paraplace',
+        imageSrc: './assets/img/cs_paraplace.jpg',
+        description: 'Constructed an M249-only map for <a href="http://store.steampowered.com/app/10/" target="_new">Counter-Strike</a>. Map showcases why you should never build enemy spawn locations within sight of each other.',
+        tags: ['Counter-Strike'],
+        date: '2002',
+        className: 'paraplace'
+    }, {
+        title: 'de_blaze',
+        imageSrc: './assets/img/de_blaze.jpg',
+        description: 'Created a bomb defusal map for <a href="http://store.steampowered.com/app/10/" target="_new">Counter-Strike</a> set in an Alaskan Telecommunications Facility. Map showcases what happens when you care more about visuals/logistics than actual gameplay.',
+        tags: ['Counter-Strike'],
+        date: '2002',
+        className: 'blaze'
+    }, {
+        title: 'ka_iceberg',
+        imageSrc: './assets/img/ka_iceberg.jpg',
+        description: 'Built my first <a href="http://store.steampowered.com/app/10/" target="_new">Counter-Strike</a> map; a knife-only map set in an iceberg. Map showcases more evidence that the first map you make is absolutely terrible.',
+        tags: ['Counter-Strike'],
+        date: '2001',
+        className: 'iceberg'
+    }]
+};
